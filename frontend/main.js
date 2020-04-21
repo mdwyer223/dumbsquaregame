bc73f7b7d6755bf533370745e7462d0cf9224dd6
Jason Proleika <proleij1@gmail.com>
2020-04-21 12:58:18 -0400

    added the ability to start the game by hovering, and give you a random square position. next is clicking on the square, adding a point, and resetting the game board

--- frontend/main.js
+++ frontend/main.js
@@ -1,9 +1,17 @@
+// 
+// 
+// I'm in the middle of commenting through everything
+
 $(document).ready(function () {
 
   $(".back-button").click(function () {
 
+    // clicking the back button disconnects from the session
+    
     console.log('Disconnecting from game service...');
     gameService.disconnect();
+    
+    // brings the page back to the main menu
 
     $(".main-menu-wrapper").removeClass("display-none");
     $(".create-game-wrapper").addClass("display-none");
@@ -11,16 +19,22 @@ $(document).ready(function () {
     $(".settings-wrapper").addClass("display-none");
     $(".game-wrapper").addClass("display-none");
 
+    // removes the back button and any associated classes
+    
     $(".back-button").addClass("display-none");
     $(".back-button").removeClass("back-create-game");
     $(".back-button").removeClass("back-join-game");
     $(".back-button").removeClass("back-settings");
     $(".back-button").removeClass("back-game");
 
+    // In game the button says "exit", so we're reverting it here
+    
     $(".back-button").text("back");
 
   });
 
+  // In game the button says "exit", so we're reverting it here
+  
   $("#create-game-menu").click(function () {
 
     $(".create-game-wrapper").removeClass("display-none");
@@ -101,21 +115,6 @@ $(document).ready(function () {
 
   });
 
-  $(".jason-square").click(function () {
-
-    let string = $(".color-red .points span").text();
-    let points = parseInt(string);
-
-    // points++;
-
-    console.log(points);
-
-    gameService.score('fakeid', 'jason', points);
-
-    // $(".color-red .points span").text(points);
-
-  });
-
   $(".matt-square").click(function () {
 
     let string = $(".color-green .points span").text();
@@ -131,8 +130,6 @@ $(document).ready(function () {
 
   });
 
-
-
   $(".chat .send-button").click(function () {
 
     let messageString = $(".chat input").val();
@@ -144,45 +141,101 @@ $(document).ready(function () {
 
     }
 
-    console.log(messageString);
+  });
 
+  var isOnDiv = false;
+
+  $(".canvas .ready").mouseenter(function () {
+    isOnDiv = true;
+  });
+
+  $(".canvas .ready").mouseleave(function () {
+    isOnDiv = false;
   });
 
 
-  //    $(".nav-button-container > div").click(function () {
-  //
-  //        let button1Text = $("#button1").html();
-  //        let button2Text = $("#button2").html();
-  //        let button3Text = $("#button3").html();
-  //        let button4Text = $("#button4").html();
-  //        let button5Text = $("#button5").html();
-  //        let button6Text = $("#button6").html();
-  //
-  //        let navButtonClass = $(this).attr("class");
-  //
-  //        if ($(".case-study").hasClass("page-displayed")) {
-  //
-  //            let str1 = $(this).attr("class");
-  //            let str2 = "active-button";
-  //
-  //    $(".nav-back-container .x").click(function () {
-  //
-  //        // removes nav
-  //
-  //        $(".navigation").removeClass("navigation-displayed");
-  //
-  //        if ($(".case-study").hasClass("page-displayed")) {
-  //
-  //            $.scrollify.disable();
-  //
-  //        } else {
-  //
-  //            $.scrollify.disable();
-  //
-  //        }
-  //
-  //    });
-  //    });
+  $(".canvas .ready").hover(function () {
+
+    let time = 399;
+
+    let interval = setInterval(function () {
+      if (time > 100 && isOnDiv === true) {
+
+        let num = time.toString();
+        let firstNum = num.charAt(0);
+        
+        $(".canvas .ready div").text(firstNum);
+        time = time - 1;
+
+      } else if (time > 100 && isOnDiv === false) {
+
+        $(".canvas .ready div").text("Hold mouse here to start");
+        clearInterval(interval);
+
+      } else {
+        
+        $(".canvas .ready").addClass("display-none");
+        $(".canvas .square").removeClass("display-none");
+    
+        let rand1 = (Math.floor(Math.random() * 100) - 50);
+        let rand2 = (Math.floor(Math.random() * 100) - 50);
+        
+        let negOrPos1 = "+"; 
+        let negOrPos2 = "+";
+        
+        if (rand1 > 40) {
+          
+          negOrPos1 = "- 52px";
+          
+        } else if (rand1 < -40) {
+          
+          negOrPos1 = "+ 52px";
+          
+        } else {
+          
+          negOrPos1 = "+ 0px";
+          
+        }
+        
+        if (rand2 > 40) {
+          
+          negOrPos2 = "- 52px";
+          
+        } else if (rand2 < -40) {
+          
+          negOrPos2 = "+ 52px";
+          
+        } else {
+          
+          negOrPos2 = "+ 0px";
+          
+        }
+        
+        let fullString1 = "calc(" + rand1 + "% " + negOrPos1 + ")";
+        let fullString2 = "calc(" + rand2 + "% " + negOrPos2 + ")";
+        
+        console.log(rand1);
+        console.log(rand2);
+        console.log(negOrPos1);
+        console.log(negOrPos2);
+        console.log(fullString1);
+        console.log(fullString2);
+        
+        $(".canvas .square").css("top", fullString1);
+        $(".canvas .square").css("left", fullString2);
+        
+        clearInterval(interval);
+
+      }
+    }, 10);
+  });
+
+
+
+
+
+
+
 
 
 });
